f82b74edb56bbc7e1d12370995c681385eb6627e
AdityaVoid <Aditya.kumar@webplat.in>
2024-09-03 10:35:31 +0530

    Scafhold

---
+++ student_api/Models/Train.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace student_api.Models
+{
+    public partial class Train
+    {
+        public string? Name { get; set; }
+        public int? TrainNo { get; set; }
+    }
+}
--- student_api/Models/demoprojectContext.cs
+++ student_api/Models/demoprojectContext.cs
@@ -17,12 +17,14 @@ namespace student_api.Models
         }
 
         public virtual DbSet<Student> Students { get; set; } = null!;
+        public virtual DbSet<Train> Trains { get; set; } = null!;
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
-
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=LAPTOP-RBUJ59QI\\MSSQLSERVER02; Database=demoproject; Trusted_Connection=True;");
             }
         }
 
@@ -45,6 +47,20 @@ namespace student_api.Models
                     .HasMaxLength(40)
                     .IsUnicode(false)
                     .HasColumnName("name");
+            });
+
+            modelBuilder.Entity<Train>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.ToTable("TRAIN");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("NAME");
+
+                entity.Property(e => e.TrainNo).HasColumnName("TRAIN_NO");
             });
 
             OnModelCreatingPartial(modelBuilder);
